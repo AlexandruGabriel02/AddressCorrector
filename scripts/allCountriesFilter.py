9447e63945f17ac51f15d4077231d9f7195162e0
Andrei Plugaru <andrei.plugaru2001@gmail.com>
2023-05-24 19:07:25 +0300

    changed hierarchy generation to using admin1codes

--- scripts/allCountriesFilter.py
+++ scripts/allCountriesFilter.py
@@ -1,25 +1,32 @@
-european_country_codes = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE"]
+european_country_codes = ["AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "MD"]
 fileName = "data/allCountries.txt"
 
 def parse_file():
-    output = open("data/allEuropeanEntities.txt", "w", encoding="utf8")
+    output = open("data/allEuropeanCities.txt", "w", encoding="utf8")
     with open(fileName, encoding="utf8") as file:
         for line in file:
             line = line.strip()
             line = line.split("\t")
+            if(line[0].strip() == '2598303'):
+                print('here')
+                for i in range(0, len(line)):
+                    print(line[i] + " ")
+           
             if line[8] not in european_country_codes:
                 continue
 
             id = line[0]
             name = line[1]
             asciiname = line[2]
-            alternatenames = line[3]
+            alternatenames = line[3].strip()
             feature_class = line[6]
-            
-            if feature_class != "P" and feature_class != "A":
+            admin2_code = line[11].strip()
+            if feature_class != "P":
                 continue
-
-            output.write(id + "\t" + name + "\t" + asciiname + "\t" + alternatenames + "\n")
+            country_code = line[8].strip()
+            admin1_code = line[10].strip()
+            admin_code = country_code + "." + admin1_code
+            output.write(id + "\t" + name + "\t" + asciiname  + "\t"+ alternatenames+ "\t" + admin_code + "\t\n")
     output.close()
 
 parse_file()
