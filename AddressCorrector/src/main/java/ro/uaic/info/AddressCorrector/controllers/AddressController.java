a2d9b1e42b1c0fe961554c582bf98172d1cf9fca
AlexandruGabriel02 <70471765+AlexandruGabriel02@users.noreply.github.com>
2023-06-02 17:32:27 +0300

    changed get with request body in get with request params

--- AddressCorrector/src/main/java/ro/uaic/info/AddressCorrector/controllers/AddressController.java
+++ AddressCorrector/src/main/java/ro/uaic/info/AddressCorrector/controllers/AddressController.java
@@ -3,10 +3,7 @@ package ro.uaic.info.AddressCorrector.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.uaic.info.AddressCorrector.models.Address;
 import ro.uaic.info.AddressCorrector.models.CorrectedAddress;
 import ro.uaic.info.AddressCorrector.services.AddressService;
@@ -21,8 +18,7 @@ public class AddressController {
     private final AddressService addressService;
     @GetMapping(path = "addresses")
     @Operation(tags = "Corrector", description = "Obtains the list of corrected addresses")
-    public List<CorrectedAddress> getCorrectedAddress(@RequestBody Address addressInput) {
-        System.out.println(addressInput.getCity());
-        return addressService.getCorrectedAddress(addressInput);
+    public List<CorrectedAddress> getCorrectedAddress(@RequestParam String country, @RequestParam String state, @RequestParam String city) {
+        return addressService.getCorrectedAddress(new Address(country, state, city));
     }
 }
